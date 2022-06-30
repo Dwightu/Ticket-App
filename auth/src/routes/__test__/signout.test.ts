73fdb7fff67cfabb799a10999b723fa522908bb2
Dwightu <yudehui1115@gmail.com>
2022-06-30 02:26:23 -0400

    Add Jest Test on Isolated Microservicres

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,22 @@
+import { response } from 'express';
+import request from 'supertest'
+import { app } from '../../app'
+
+
+it('returns a 201 on successful signing out', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .get('/api/users/signout')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200)
+})
