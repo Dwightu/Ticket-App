5a095328537193ce84c08d8a9c5f534acbc1f6e8
Dwightu <yudehui1115@gmail.com>
2022-07-07 02:00:20 -0400

    Update

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../app'
+
+it('Has a route handler listening to /api/tickets for post requests', async () => {
+    const response = await request(app).post('/api/tickets').send({});
+    expect(response.status).not.toEqual(404);
+})
+
+it('Can only be accessed if the user is signed in', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401)
+})
+
+it('returns a status other than 401 if the user is signed in', async () => {
+    const response = await request(app).post('/api/tickets').send({})
+    expect(response.status).not.toEqual(401);
+})
+
+it('Has a route handler listening to /api/tickets for post requests', async () => {
+
+})
